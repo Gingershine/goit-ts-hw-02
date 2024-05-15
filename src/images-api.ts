713d7a5c3bd580f89e5ac4d3cab26fd22722cb6c
Gingershine <142210638+Gingershine@users.noreply.github.com>
2024-05-15 12:29:44 +0300

    add axios interface

--- src/images-api.ts
+++ src/images-api.ts
@@ -1,11 +1,19 @@
 import axios, { AxiosResponse } from 'axios';
+import { Image } from './types';
 
 axios.defaults.baseURL = 'https://api.unsplash.com';
 
 const API_KEY = 'Sutydj1T35CniTGuzyBONubxGK6Q83QuE5gDMNUCKJg'
 
-const fetchImagesByName = async (query: string, currentPage: number): Promise<AxiosResponse<any>> => {
+interface ImageResponse {
+  total: number;
+  total_pages: number;
+  results: Image[];
+}
+
+const fetchImagesByName = async (query: string, currentPage: number): Promise<AxiosResponse<ImageResponse>> => {
     const response = await axios.get(`search/photos?client_id=${API_KEY}&page=${currentPage}&query=${query}`);
+    console.log(response);
     
     return response;
 }
